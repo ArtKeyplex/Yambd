19a69649f19b53c6474f5fb7d4b5ce6d93543562
Антон Карпов <3656387@gmail.com>
2022-10-02 17:04:08 +0300

    внесены правки после pytest в Review, Comments

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,12 +1,13 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsAuthorOrModeratorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            obj.author == request.user
-            or request.method in permissions.SAFE_METHODS
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.is_moderator
         )
 
 
